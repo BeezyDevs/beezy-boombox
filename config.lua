2d42bf14e547740956cce5cfc3da167863033b35
BeezyDevs <dsrpbeezy@gmail.com>
2026-03-05 21:36:14 -0600

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,22 +1,4 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <link rel="stylesheet" href="style.css">
-</head>
-<body>
-    <div id="menu" style="display:none;">
-        <h2>Beezy Boombox</h2>
+Config = {}
 
-        <input id="url" type="text" placeholder="YouTube URL">
-
-        <label>Volume</label>
-        <input id="volume" type="range" min="0" max="100" value="50">
-
-        <button onclick="play()">Play</button>
-        <button onclick="stop()">Stop</button>
-        <button onclick="closeMenu()">Close</button>
-    </div>
-
-    <script src="script.js"></script>
-</body>
-</html>
+Config.Item = "beezy_boombox"
+Config.Model = "prop_boombox_01"
